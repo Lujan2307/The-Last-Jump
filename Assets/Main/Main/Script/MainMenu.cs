2a9ea20b1dc33bba3c8f5471ab7eaa4c590d3fd6
Kiwi-ateh <batehortua787@soyudemedellin.edu.co>
2026-09-17 10:16:10 -0500

    Funcionalidades botones

---
+++ Assets/Main/Main/Script/MainMenu.cs
@@ -0,0 +1,38 @@
+using Unity.VectorGraphics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
+
+    public void OpenOptionsPanel()
+    {
+
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void OpenMenuPanel()
+    {
+
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Mapa (Fácil");
+
+
+    }
+}
