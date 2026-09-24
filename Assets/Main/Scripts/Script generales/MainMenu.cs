a163fd299d1b0416e9eb572a01a2797cf1523810
Lujan <166866321+Lujan2307@users.noreply.github.com>
2026-09-24 09:51:10 -0500

    ultimos detalles

--- Assets/Main/Scripts/Script generales/MainMenu.cs
+++ Assets/Main/Scripts/Script generales/MainMenu.cs
@@ -31,7 +31,7 @@ public class MainMenu : MonoBehaviour
 
     public void Play()
     {
-        SceneManager.LoadScene("Mapa (Fácil)");
+        SceneManager.LoadScene("Mapa (Fácil) 1");
 
 
     }
